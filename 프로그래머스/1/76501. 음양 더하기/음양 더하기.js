9114cd3c7cdf8b624a0de2d731f142c5d5a6a4cf
LIHA SEO <122168823+liha-engineer@users.noreply.github.com>
2024-08-13 17:08:09 +0900

    [level 1] Title: 음양 더하기, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -0,0 +1,13 @@
+function solution(absolutes, signs) {
+    var answer = 0;
+    
+    for (let i = 0; i < absolutes.length ; i++) {
+        
+        if (!signs[i]) {
+        absolutes[i] = -absolutes[i];
+        } 
+        answer += absolutes[i];
+
+    }
+    return answer;
+}
