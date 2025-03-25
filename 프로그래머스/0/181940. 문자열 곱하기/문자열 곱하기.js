6ef44ad82b4d07833717b7db9c482844f92d7d28
LIHA SEO <122168823+liha-engineer@users.noreply.github.com>
2025-03-25 17:12:43 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.js
@@ -0,0 +1,8 @@
+const repeatString = (my_string, k) => {
+    const repeatedString = my_string.repeat(k)
+    return repeatedString;
+}
+
+function solution(my_string, k) {
+    return repeatString(my_string, k);
+}
