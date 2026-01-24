0ceb77608cfdd0f845a21daf32e549a7602b181a
LIHA SEO <122168823+liha-engineer@users.noreply.github.com>
2026-01-25 00:42:02 +0900

    [Bronze V] Title: A+B, Time: 48 ms, Memory: 5500 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace backjune {
+    public class Program {
+        public static void Main(string[] args) {
+            string input = Console.ReadLine();
+            int a = Int32.Parse(input.Split(" ")[0]);
+            int b = Int32.Parse(input.Split(" ")[1]);
+            int answer = a + b;
+            Console.WriteLine(answer);
+        }
+    }
+}
