c471888ab2291febd797249804dbe84dc9fae167
LIHA SEO <122168823+liha-engineer@users.noreply.github.com>
2026-02-08 21:31:54 +0900

    Time: 29 ms (72.1%), Space: 67.8 MB (57.04%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -3,18 +3,13 @@
  * @return {string[][]}
  */
 const groupAnagrams = function(strs) {
-    if (!strs || strs.length === 0 || strs.length === 1) return [strs];
-
+    if (!strs || strs.length === 1) return [strs];
     const map = new Map()
 
-    for (const word of strs) {
-        const key = word.split("").sort().join("")
-
-        if(!map.has(key)) map.set(key, [])
-        map.get(key).push(word)
+    for (word of strs) {
+        const sorted = word.split("").sort().join("")
+        if(!map.has(sorted)) map.set(sorted, [])
+        map.get(sorted).push(word)
     }
-
-    const answer = Array.from(map.values())
-    return answer
-
+    return [...map.values()]
 };
